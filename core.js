6c2e40c4a8d1dcf9a6b2c64e0ca22385938883ea
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2024-12-20 22:17:14 +0100

    more on event and event emitters

--- core.js
+++ core.js
@@ -1,37 +1,14 @@
-const path = require("node:path");
-const readline = require("node:readline");
-const v8 = require("node:v8");
-console.log(path.basename(__filename));
-let dirUploads = path.join(__dirname, "www", "files", "uploads");
-
-// console.log(dirUploads);
-// console.log(v8.getHeapStatistics());
-
-let rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const collectAnswers = require("./lib/collectAnswers");
 
 const questions = [
   "what is your name?",
   "where do you live?",
   "what are you going to do with node js?",
 ];
-
-function collectAnswers(questions, done) {
-  const answers = [];
-  const questionAnswered = (answer) => {
-    answers.push(answer.trim());
-    if (answers.length < questions.length) {
-      rl.question(questions[answers.length], questionAnswered);
-    } else {
-      return done(answers);
-    }
-  };
-  rl.question(questions[0], questionAnswered);
-}
-collectAnswers(questions, (answers) => {
+const answersEvent = collectAnswers(questions, (answers) => {
   console.log(`thank you for your answers !`);
   console.log(answers);
-  rl.close();
+  process.exit();
 });
+
+answersEvent.on("answer", (answer) => console.log(`the answer is ${answer}`));
